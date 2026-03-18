208758c17ed453918003000f8478f27e0c90e019
Saulo Rodrigues <saulorodm@gmail.com>
2026-03-18 11:25:18 -0300

    chore: altera link do regulamento

--- src/lib/regulamento.ts
+++ src/lib/regulamento.ts
@@ -1,5 +1,5 @@
 export const REGULAMENTO_URL =
-  "https://drive.google.com/file/d/1k5_lngnyQkzArg9hx4ySrqPnkX581fkK/view?usp=sharing";
+  "https://drive.google.com/file/d/14IFq1CFH_OhxNVzfCAikDFpj9cJN3-SV/view?usp=sharing";
 
 export function abrirRegulamentoEmNovaAba() {
   window.open(REGULAMENTO_URL, "_blank", "noopener,noreferrer");
